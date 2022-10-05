13c60afc8ec9e0efa9cbd5866cf322e11de27dfc
alovyannikov <radiant42@yandex.ru>
2022-10-05 11:01:48 +0500

    added swagger, described examples

--- back/src/main/java/com/summerdev/back/controller/PersonProfileController.java
+++ back/src/main/java/com/summerdev/back/controller/PersonProfileController.java
@@ -3,20 +3,25 @@ package com.summerdev.back.controller;
 import com.summerdev.back.request.UpdateCurrencyRequest;
 import com.summerdev.back.response.PersonProfileResponse;
 import com.summerdev.back.service.PersonProfileService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("api/web/")
+@RequestMapping("/api/web")
 @RestController
 public class PersonProfileController {
     private final PersonProfileService personProfileService;
 
+    @ApiOperation(value = "Получить сведения о портфеле пользователя", notes = "Получить сведения о портфеле пользователя")
     @GetMapping("/profile")
     public PersonProfileResponse get() {
         return personProfileService.getPersonProfile();
     }
 
+    @ApiOperation(value = "Конвертировать валюту", notes = "Конвертировать валюту")
+    @ApiImplicitParam(name = "request", value = "Параметры конвертации валюты", required = true, dataType = "UpdateCurrencyRequest")
     @PostMapping("/profile/convertCurrency")
     public PersonProfileResponse convertCurrency(@RequestBody UpdateCurrencyRequest request) {
         return personProfileService.updateProfileCurrency(request);
--- back/src/main/java/com/summerdev/back/request/UpdateCurrencyRequest.java
+++ back/src/main/java/com/summerdev/back/request/UpdateCurrencyRequest.java
@@ -1,12 +1,18 @@
 package com.summerdev.back.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class UpdateCurrencyRequest {
+    @ApiModelProperty(example = "1")
     private final int currencyTypeIdFrom;
+
+    @ApiModelProperty(example = "2")
     private final int currencyTypeIdTo;
+
+    @ApiModelProperty(example = "500")
     private final long currencyValueFrom;
 }
